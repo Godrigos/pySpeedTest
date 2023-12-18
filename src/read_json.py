298d1fb742cf3c5fbadf3f4f47b8a571cebf3239
Rodrigo Aluizio <r.aluizio@gmail.com>
2023-12-18 08:58:14 -0300

    Close files after every open

--- src/read_json.py
+++ src/read_json.py
@@ -19,6 +19,7 @@ def results_to_tsv(filepath: str) -> None:
     try:
         with open(f'{directory}/data/results.json', mode = 'rt') as file:
             data = json.load(file)
+        file.close()
     except:
         exit(f'{datetime.now()} - Error reading file results.json')
     else:
@@ -26,11 +27,13 @@ def results_to_tsv(filepath: str) -> None:
             if not exists(filepath):
                 with open(filepath, mode = 'w+') as file:
                     file.write('Date\tISP\tIP\tServer\tLocation	Latency (ms)\tDownload Speed (Mbps)\tDownload Latency (ms)\tUpload Speed (Mbps)\tUpload Latency (ms)\tPacket Loss (%)\tId\n')
+                file.close()
         except:
             exit(f'{datetime.now()} - Error creating file {filepath}')
 
         try:
             with open(filepath, mode = 'at') as file:
                 file.write(f"{tz_converter(data['timestamp'])}\t{data['isp']}\t{data['interface']['externalIp']}\t{data['server']['name']}\t{data['server']['location']}\t{data['ping']['latency']}\t{data['download']['bandwidth']/125000}\t{data['download']['latency']['iqm']}\t{data['upload']['bandwidth']/125000}\t{data['upload']['latency']['iqm']}\t{data['packetLoss']}\t{data['result']['id']}\n")
+            file.close()
         except:
             exit(f'{datetime.now()} - Error writing data to {filepath}')
